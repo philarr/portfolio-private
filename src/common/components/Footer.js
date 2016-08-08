ed8c53a1702e6ec22c224cb92365b9c197bb131f
Philip Chung <philarlol@gmail.com>
2016-08-08 10:27:41 -0700

    Footer

---
+++ src/common/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+class Footer extends React.Component {
+
+
+
+	render() {
+
+
+
+		return (		 	 
+
+				<div className="footer">
+					<div className="inner">
+				 
+						<div className="footer-left">
+							<h4 href="mailto:contact@pmhc.co" className="icon mail">contact@pmhc.co</h4>
+			 			
+
+			 			</div>
+		 				<div className="footer-right">
+		 				
+		 						<a href="https://ca.linkedin.com/in/philip-chung-6955b7106" target="_blank">LinkedIn</a>
+		 						&mdash;
+		 						<a href="https://twitter.com/pmhc_" target="_blank">Twitter</a>
+		 						&mdash;
+		 						<a href="http://www.instagram.com/pmhc_" target="_blank">Instagram</a>
+		 					
+		 				</div>
+				 
+			 		</div>
+		 		</div>
+	 	 
+
+
+		);
+	}
+}
+
+
+export default Footer
